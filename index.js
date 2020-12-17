c4cc69699999b8a9230bfb5b5d9e7fc9e0a04ad2
William (Wgyt) <github@wgyt.tk>
2020-12-17 16:48:09 +0000

    more headroom

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 const mustacheExpress = require('mustache-express');
 app.engine('htm', mustacheExpress());
-app.set('views', './');
+app.set('views', './views');
 app.set('view engine', 'htm');
+// you can go to / to show index.html
 app.get('/', (req, res) => {
+	url = req.query.url
+		res.render('index', {
+			url: url
+    });
+});
+// you can go to /repl to show repl it login
+app.get('/repl', (req, res) => {
 	if (req.get('X-Replit-User-Id')) {
     // Templating time!
 		res.render('welcome', {
       userid: req.get('X-Replit-User-Id'),
       username: req.get('X-Replit-User-Name'),
       userroles: req.get('X-Replit-User-Roles'),
-			url: req.query.url
+			url: url
     });
 	} else {
     // Log-in prompt.
-		res.sendFile(__dirname+"/new.html");
+		res.sendFile(__dirname+"/views/new.htm");
 	}
-});
-app.get('/welcome.js', (req, res) => {
-	res.sendFile(__dirname+"/welcome.js");
 });
 /// Listening */
 app.listen(8080);
