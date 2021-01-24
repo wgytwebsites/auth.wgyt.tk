7e309e39810b67b3d51e1b181137a5fc09fcd4f9
Wgyt (William) <w.horning27@gmail.com>
2021-01-24 17:37:06 +0000

    this is the basis of v1, this includes google and github login that are partially done

--- index.js
+++ index.js
@@ -1,31 +1,90 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 const mustacheExpress = require('mustache-express');
-app.engine('htm', mustacheExpress());
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+var urlencodedParser = bodyParser.urlencoded({
+	extended: false
+})
+app.use(cookieParser());
+app.engine('html', mustacheExpress());
 app.set('views', './views');
-app.set('view engine', 'htm');
-// you can go to / to show index.html
+app.set('view engine', 'html');
+// google auth id token setup
+const {
+	OAuth2Client
+} = require('google-auth-library');
+const client = new OAuth2Client(process.env.googleAppUrl);
+// redirect to v0
 app.get('/', (req, res) => {
-	url = req.query.url
-		res.render('index', {
-			url: url
-    });
+	res.set('location', '/v0/get?url=' + req.query.url);
+	res.status(301).send()
 });
-// you can go to /repl to show repl it login
 app.get('/repl', (req, res) => {
+	res.set('location', '/v0/get/repl?url=' + req.query.url);
+	res.status(301).send()
+});
+// v0 api
+app.get('/v0/get/', (req, res) => {
+	url = req.query.url
+	res.render('index', {
+		url: url
+	});
+});
+app.get('/v0/get/repl', (req, res) => {
 	if (req.get('X-Replit-User-Id')) {
-    // Templating time!
 		res.render('welcome', {
-      userid: req.get('X-Replit-User-Id'),
-      username: req.get('X-Replit-User-Name'),
-      userroles: req.get('X-Replit-User-Roles'),
-			url: req.query.url
-    });
+			userid: req.get('X-Replit-User-Id')
+			, username: req.get('X-Replit-User-Name')
+			, userroles: req.get('X-Replit-User-Roles')
+			, url: req.query.url
+		});
 	} else {
-    // Log-in prompt.
-		res.sendFile(__dirname+"/views/new.htm");
+		res.sendFile(__dirname + "/views/new.html");
 	}
 });
-/// Listening */
+// v1 beta api
+app.get('/v1/json/', (req, res) => {
+	// paramater test url: https://auth.wgyt.tk/v1/json?redirectURL=wgyt&postDataURL=wgyt&whatDataOBJ=%22name%22:true,%22id%22:true,%22roles%22:true&plfmTypeOBJ=%22replit%22:true&appNameSTRG=wgyt&appMailSTRG=wgyt@wgyt.tk&appDvlpSTRG=wgyt
+	res.send(`{
+    "redirectURL":"${req.query.redirectURL}",
+    "postDataURL":"${req.query.postDataURL}",
+    "whatDataOBJ":{${req.query.whatDataOBJ}},
+    "plfmTypeOBJ":{${req.query.plfmTypeOBJ}},
+    "appNameSTRG":"${req.query.appNameSTRG}",
+    "appMailSTRG":"${req.query.appMailSTRG}",
+    "appDvlpSTRG":"${req.query.appDvlpSTRG}",
+    "timestamp":${Date.now()}
+  }`)
+});
+app.get('/v1', (req, res) => {
+	// TODO: SET UP SETTINGS USING ABOVE PARAMATERS
+	res.sendFile(__dirname + "/views/index-v1.html");
+});
+app.post('/v1/backend/google', urlencodedParser, (req) => {
+	// google verify id token first
+	var PAYLOAD
+	async function verify(idtoken) {
+		const ticket = await client.verifyIdToken({
+			idToken: idtoken
+			, audience: process.env.googleAppUrl
+		, });
+		// get google payload
+		payload = ticket.getPayload()
+			// TODO: RETURN THE PAYLOAD TO THE CLIENT TO POST THE SERVER OR DO IT HERE //
+	}
+	verify(req.body.idtoken).catch(console.error)
+		// req.body.idtoken is sent by the client in views/index-v1.html in the onSignIn function
+});
+app.get('/v1/backend/github', (req, res) => {
+	res.send(`{"code":"${req.query.code}"}`);
+	// state is req.query.state
+	// code is req.query.code
+	// todo: do https://docs.github.com/en/developers/apps/identifying-and-authorizing-users-for-github-apps#2-users-are-redirected-back-to-your-site-by-github
+});
 app.listen(8080);
